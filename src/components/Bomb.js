b0cd8619a75c5714956e8673e0684eec119e377a
Thi quynh Vo <yenfrance1703@gmail.com>
2018-04-20 09:49:32 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -5,8 +5,14 @@ class Bomb extends React.Component{
   construction(props){
     super();
     this.state = {
-      
+      secondsLeft: props.initialCount
     }
-    
+
+  }
+  render (){
+    const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before i go boom!`;
+    return (
+      <div> {message}</div>
+    )
   }
 }
