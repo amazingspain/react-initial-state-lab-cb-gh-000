498e2e63aa4cafd53d06c653ad62f4ce7b98467f
Thi quynh Vo <yenfrance1703@gmail.com>
2018-04-20 09:39:11 +0000

    automatically backed up by learn

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -3,6 +3,15 @@ import React from 'react';
 export default class ImageSlider extends React.Component {
   constructor(){
     super();
+    this.state = {
+      currentSlideIndex: 0
+    }
   }
   
+}
+render(){
+  return (
+    <h1> I am on slide {this.sate.currentSlideIndex}</h1>
+  )
+}
 }
